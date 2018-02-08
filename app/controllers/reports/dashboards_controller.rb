ec1934827295d63f4abec8747127e5e4365492d5
alban grouas <agrouas@hotmail.fr>
2018-02-08 17:18:15 +0100

    adding changes

--- app/controllers/reports/dashboards_controller.rb
+++ app/controllers/reports/dashboards_controller.rb
@@ -5,6 +5,25 @@ module Reports
   class DashboardsController < ApplicationController
     def show
       @dashboard = ReportDashboardFacade.new(params[:report_id])
+      unless current_prospect.company.report.id == params[:report_id] || current_admin_user.nil?
+        redirect_to root_path
+      end
+      # @prospect_pdf = issue_id << ".pdf"
+      respond_to  do |format|
+        format.html
+        format.pdf do
+          # chart_hash = @issue.chart.split('"')
+          # @mainChart = chart_hash[1]
+          # @linkedinChart = chart_hash[3]
+          # mailchimp_hash = @issue.mailchimp.split('"')
+          # @camembert = mailchimp_hash[1]
+          # @historique = mailchimp_hash[3]
+          pdf = render :pdf => "Dashboard-sapin",
+                       :viewport_size => '1280x1024',
+                       :layout => "pdf_layout.html.erb",
+                       :disable_javascript => false
+        end
+      end
     end
   end
 end
